dffdf49ff45e588bee9d43eecd12afc7dae2a2d2
Gayane Grigoryan <44817376+Gayane05@users.noreply.github.com>
2019-09-08 19:07:33 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QtWidgets>
+#include "MouseObserver.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MouseObserver wgt;
+
+    wgt.resize(250,130);
+    wgt.show();
+
+    return a.exec();
+}
